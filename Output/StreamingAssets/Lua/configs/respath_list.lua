e3756f1c8c1aba7fb3fa952f6c27c07055d79eec
libyyu <389465209@qq.com>
2017-02-18 13:04:42 +0800

    export assets bundles

--- Output/StreamingAssets/Lua/configs/respath_list.lua
+++ Output/StreamingAssets/Lua/configs/respath_list.lua
@@ -1,11 +1,11 @@
 local respath_list = 
 {
-	ConsoleUI = "Assets/Arts/Arts/Prefab/ConsoleUI.prefab",
-	LoadingUI = "Assets/Arts/Arts/Prefab/LoadingUI.prefab",
-	LoginUI = "Assets/Arts/Arts/Prefab/LoginUI.prefab",
-	MsgBoxUI = "Assets/Arts/Arts/Prefab/MsgBoxUI.prefab",
-	ServerListUI = "Assets/Arts/Arts/Prefab/ServerListUI.prefab",
-	BackgroundMusic = "Assets/Arts/Arts/Sound/background.mp3",
+	ConsoleUI = "Assets/Arts/Prefab/ConsoleUI.prefab",
+	LoadingUI = "Assets/Arts/Prefab/LoadingUI.prefab",
+	LoginUI = "Assets/Arts/Prefab/LoginUI.prefab",
+	MsgBoxUI = "Assets/Arts/Prefab/MsgBoxUI.prefab",
+	ServerListUI = "Assets/Arts/Prefab/ServerListUI.prefab",
+	BackgroundMusic = "Assets/Arts/Sound/background.mp3",
 }
 
 return respath_list
