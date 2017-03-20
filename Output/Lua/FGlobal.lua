177c861324f6f38ba179fe72a7aee08b009f4d40
libyyu <389465209@qq.com>
2017-03-21 00:32:13 +0800

    split

--- Output/Lua/FGlobal.lua
+++ Output/Lua/FGlobal.lua
@@ -150,11 +150,3 @@ function NewByteBuffer(data)
 		return ByteBuffer(data)
 	end
 end
-
--- 分隔字符串
-function string:split(sep)
-    local sep, fields = sep or "\t", {}
-    local pattern = string.format("([^%s]+)", sep)
-    self:gsub(pattern, function(c) fields[#fields+1] = c end)
-    return fields
-end
